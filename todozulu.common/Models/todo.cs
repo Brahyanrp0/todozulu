bd60c1e448da2458509d2b580e2b5bc91463bf3f
BRAHYAN RESTREPO TORRES <brahyanrestrepo187307@correo.itm.edu.co>
2021-08-24 14:08:23 -0500

    Add entity

---
+++ todozulu.common/Models/todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todozulu.common.Models
+{
+    public class todo
+    {
+
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
